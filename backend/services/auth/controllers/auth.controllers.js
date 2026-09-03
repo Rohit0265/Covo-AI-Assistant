dd53bd515016500dd84a89a9a869a2cb898d1643
Rohit0265 <rohitmathur00981@gmail.com>
2026-09-03 21:07:08 +0530

    feat: implement session-based authentication with Redis storage and add logout functionality and dev script

--- backend/services/auth/controllers/auth.controllers.js
+++ backend/services/auth/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import { getAuth } from "firebase-admin/auth";
 import {app} from "../config/firebase.js";
 import User from "../models/users.model.js";
+import redis from "../../../shared/redis/redis.js";
 
 export const login = async(req,res)=>{
     try {
@@ -19,7 +20,13 @@ export const login = async(req,res)=>{
         }
 
         const sessionId = crypto.randomUUID();
-
+        await redis.set(`session:${sessionId}`, JSON.stringify({
+            userId:user._id,
+            name:user.name,
+            email:user.email,
+            avatar:user.avatar
+            
+        }),"EX", 7 * 24 * 60 * 60);
         res.cookie('session', sessionId, {
             httpOnly: true,
             secure:true,
@@ -31,5 +38,17 @@ export const login = async(req,res)=>{
 
     } catch (error) {
         return res.status(500).json({ message: `login error ${error}` });
+    }
+}
+
+
+export const logout = async(req,res)=>{
+    try {
+        const sessionId = req.cookies.session;
+        await redis.del(`session:${sessionId}`);
+        res.clearCookie('session');
+        return res.status(200).json({ message: 'Logout successful' });
+    } catch (error) {
+        return res.status(500).json({ message: `logout error ${error}` });
     }
 }
--- backend/services/auth/routes/auth.routes.js
+++ backend/services/auth/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login } from "../controllers/auth.controllers.js";
+import { login,logout } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.get("/logout", logout);
 
 export default router;
