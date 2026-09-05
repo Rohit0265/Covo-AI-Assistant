309b28cb474abd5a02f27572bd2e7fef7586a646
Rohit0265 <rohitmathur00981@gmail.com>
2026-09-05 15:06:48 +0530

    changes

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use("/api/auth",proxy(process.env.AUTH_SERVICE_URL));
 app.get("/api/me",protect,getCurrentUser)
 app.get("/api/chat",protect,proxyHeader(process.env.CHAT_SERVICE_URL))
+app.get("/api/agent",protect,proxy(process.env.AGENT_SERVICE_URL))
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
