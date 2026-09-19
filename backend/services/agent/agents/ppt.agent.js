7fc7088a48e52c52e2a2555021284968ec513c9a
Rohit0265 <rohitmathur00981@gmail.com>
2026-09-19 23:03:38 +0530

    feat: implement PPT generation with S3 upload functionality; add generatePPT utility and update dependencies

--- backend/services/agent/agents/ppt.agent.js
+++ backend/services/agent/agents/ppt.agent.js
@@ -1,4 +1,7 @@
 import { getModel } from "../config/llmmodel.js";
+import generatePPT from "../utils/generatePPT.js";
+import { getFiles } from "../utils/getFroms3.js";
+import { uploadToS3 } from "../utils/uploadToS3.js";
 
 
 export const ppt = async (state) => {
@@ -38,10 +41,32 @@ ${state.prompt}
     `
 
     const res = await llm.invoke(prompt)
-    
+    const data = JSON.parse(res.content)
+    const ppt = generatePPT(data)
+    const buffer = await ppt.write({
+      outputType:"nodebuffer"
+    })
 
+    const filename = `ppt-${Date.now()}.pptx`
+    await uploadToS3(buffer,filename,"application/vnd.openxmlformats-officedocument.presentationml.presentation")
+
+    const downloadUrl = await getFiles(filename,24*60*60)
+
+    return {
+      ...state,
+      aiResponse: `## ✅ Presentation Generated
+
+**${data.title}**
+
+📥 [Download PPT](${downloadUrl})
+
+*Link expires in 10 minutes.*`
+    }
 
   } catch (error) {
-    
+    console.log(error)
+    return {
+      ...state,aiResponse:"Failed to generate ppt"
+    }
   }
 };
